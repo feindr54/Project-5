5ac2af794233b4d6d5de6b4c43667ac570edb8ee
qasim-a <ali170@purdue.edu>
2021-12-07 18:48:32 -0500

    Added client stuff and fixed settings buttons in CourseStudent and CourseTeacher classes

--- src/pages/CourseStudent.java
+++ src/pages/CourseStudent.java
@@ -1,3 +1,6 @@
+package pages;
+import networking.ActualClient;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -15,6 +18,8 @@ import java.util.*;
 public class CourseStudent extends JComponent {
 
     // TODO - remove all static references - CX
+    ActualClient client;
+    Container content;
 
     ArrayList<String> forumsArr;
     JPanel defaultPanel;
@@ -43,7 +48,8 @@ public class CourseStudent extends JComponent {
                 forums.setSelectedIndex(0);
             }
             if (e.getSource() == settingsButton) {
-                //settings.access()
+                client.getPageStack().push("settings");
+                client.getCl().show(client.getMainPanel(), "settings");
             }
             if (e.getSource() == backButton) {
                 //LMS.access()
@@ -51,7 +57,9 @@ public class CourseStudent extends JComponent {
         }
     };
 
-    public CourseStudent() {
+    public CourseStudent(ActualClient client) {
+        this.client = client;
+
         forumsArr = new ArrayList<>();
         //TODO replace with getForums()
         forumsArr.add("Forum 1");
@@ -59,7 +67,7 @@ public class CourseStudent extends JComponent {
         forumsArr.add("Forum 3");
         forumsArr.add("Forum 4");
         //TODO replace with getCourseName()
-        Container content = new Container;
+        content = new Container();
         content.setLayout(new BorderLayout());
 
         //header and back/settings buttons
