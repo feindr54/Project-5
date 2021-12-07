24bfb9a6d5c224d975a6773cb5a311e8718ebf3b
qasim-a <ali170@purdue.edu>
2021-12-07 17:21:39 -0500

    Added some stuff to methods in CourseStudent and CourseTeacher classes

--- CourseStudent.java
+++ CourseStudent.java
@@ -39,6 +39,12 @@ public class CourseStudent extends JComponent implements Runnable {
                 //if true, forum.access()
                 //else show error message
             }
+            if (e.getSource() == backButton) {
+                //settings.access()
+            }
+            if (e.getSource() == backButton) {
+                //LMS.access()
+            }
         }
     };
 
@@ -48,14 +54,15 @@ public class CourseStudent extends JComponent implements Runnable {
 
     public void run() {
         forumsArr = new ArrayList<>();
+        //TODO replace with getForums()
         forumsArr.add("Forum 1");
         forumsArr.add("Forum 2");
         forumsArr.add("Forum 3");
         forumsArr.add("Forum 4");
         frame = new JFrame("courseName");
+        //TODO replace with getCourseName()
         Container content = frame.getContentPane();
         content.setLayout(new BorderLayout());
-        CourseStudent course = new CourseStudent();
 
         //header and back/settings buttons
         defaultPanel = new JPanel();
@@ -63,6 +70,7 @@ public class CourseStudent extends JComponent implements Runnable {
         backButton.addActionListener(actionListener);
         defaultPanel.add(backButton);
         welcomeLabel = new JLabel("Welcome to " + "courseName" + "!");
+        //TODO replace with getCourseName()
         defaultPanel.add(welcomeLabel);
         settingsButton = new JButton("Settings");
         settingsButton.addActionListener(actionListener);
@@ -80,6 +88,7 @@ public class CourseStudent extends JComponent implements Runnable {
 
         accessPanel.add(gradeSentence, c);
         gradeCourse = new JLabel("grade");
+        //TODO replace with getGrade()
 
         c.weightx = 1;
         c.gridx = 1;
